e1ec2f757e3db45264aea8892d43975771f7d490
Ian Cook Westgate and Nuala Gray <nuala@>
2019-01-17 15:51:51 -0800

    add variables, fix persistent graphic problem, start adding values

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,25 @@
 // function popularity(fage,ffollow){
 //   return ffollow/fage;
 // }
-
-
+// var points;
+// var toggle;
+// var factor;
+// function pointfunc(ftoggle,ffactor){
+//     return (((-1)**ftoggle)*ffactor)
+// }
+var grandTotal
 
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
     var nameInput = $("input#name").val();
-    var ageInput = $("input#age").val();
-    var genderInput= $("select#gender").val();
-    var immigrationInput = $("select#immigration").val();
-    var climateChangeInput = $("select#climateChange").val();
-    var foreignPolicyInput = $("select#foreignPolicy").val();
-    var newsSourceInput = $("select#newsSource").val();
+    var ageInput = parseInt($("input#age").val());
+    var genderInput= parseInt($("select#gender").val());
+    var immigrationInput = parseInt($("select#immigration").val());
+    var climateChangeInput = parseInt($("select#climateChange").val());
+    var foreignPolicyInput = parseInt($("select#foreignPolicy").val());
+    var newsSourceInput = parseInt($("select#newsSource").val());
 
-
+// console.log(genderInput)
     var pAge = parseInt(ageInput);
     // var pFollowers = parseInt(followersInput);
 
@@ -31,12 +36,12 @@ $(document).ready(function() {
     event.preventDefault();
 
     // var pop = popularity (pAge,pFollowers);
-
+// points = points + pointfunc(toggle,factor);
 
     // $(".pop").text(pop);
 
   });
 
-
+grandTotal = (ageInput * .2) + genderInput + immigrationInput + climateChangeInput + foreignPolicyInput + newsSourceInput;
 
 });
