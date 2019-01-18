df142d092949375c998b57bde509ca10fa685fe2
Ian Cook Westgate and Nuala Gray <nuala@>
2019-01-17 16:44:23 -0800

    Mostly functioning code.

--- js/scripts.js
+++ js/scripts.js
@@ -7,10 +7,14 @@
 // function pointfunc(ftoggle,ffactor){
 //     return (((-1)**ftoggle)*ffactor)
 // }
-var grandTotal
+
 
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
+
+    event.preventDefault();
+
+    var grandTotal = 0;
     var nameInput = $("input#name").val();
     var ageInput = parseInt($("input#age").val());
     var genderInput= parseInt($("select#gender").val());
@@ -20,20 +24,37 @@ $(document).ready(function() {
     var newsSourceInput = parseInt($("select#newsSource").val());
 
 // console.log(genderInput)
-    var pAge = parseInt(ageInput);
+    // var pAge = parseInt(ageInput);
     // var pFollowers = parseInt(followersInput);
 
-
     $(".name").text(nameInput);
-    $(".age").text(pAge);
+    $(".age").text(ageInput);
     $(".gender").text(genderInput);
     $(".immigration").text(immigrationInput);
     $(".climateChange").text(climateChangeInput);
     $(".foreignPolicy").text(foreignPolicyInput);
     $(".newsSource").text(newsSourceInput);
 
-    $("#story1").show();
-    event.preventDefault();
+
+    grandTotal = (ageInput * .1) + genderInput + immigrationInput + climateChangeInput + foreignPolicyInput + newsSourceInput;
+
+
+
+
+// If gender === "Gandalf" && immigration === "Keep the ORCS OUT!" &&
+
+    if (grandTotal < -5){
+      $("#story1").show()
+    } else if (grandTotal < 15) {
+      $("#story2").show()
+    } else {
+      $("#story3").show()};
+
+
+
+
+
+
 
     // var pop = popularity (pAge,pFollowers);
 // points = points + pointfunc(toggle,factor);
@@ -42,6 +63,8 @@ $(document).ready(function() {
 
   });
 
-grandTotal = (ageInput * .2) + genderInput + immigrationInput + climateChangeInput + foreignPolicyInput + newsSourceInput;
+
+
+
 
 });
